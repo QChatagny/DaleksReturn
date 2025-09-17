e85cc3ff840d4cbcddf039e02d178d2a39584ffe
Quentin Chatagny <e.qchatagny2@etu.cvm.qc.ca>
2025-09-16 21:39:32 -0400

    added Character class attributes

---
+++ Characters.py
@@ -0,0 +1,19 @@
+
+class Character:
+  
+  def __init__(self, posX, posY):
+    self.posX = posX
+    self.posY = posY
+
+
+c = Character(1,2)
+
+if (c.posX == 1):
+  print("you got it")
+
+else: 
+  print('not a goot one')
+
+
+
+
